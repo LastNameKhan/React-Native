d143a15a04534c15d9f4210fb32324ea01e95a38
LastNameKhan <lastnamekhan47@gmail.com>
2023-02-02 17:01:21 +0530

    Third Party Auth Google, Facebok, GitHub,LinkedIn.

---
+++ ThirdPartyAuth/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface IProps {}
+
+interface IState {}
+
+class App extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
